d6b79f088589db3a8f43a0d06552e3a9f1cd8201
Robert Quitt <robertquitt@gmail.com>
2018-04-24 20:42:45 -0700

    Anotha hard fork

--- verify-consistency.sh
+++ verify-consistency.sh
@@ -2,10 +2,11 @@
 
 TOTAL_CASH=1000000
 CURRENT_CASH=0
-FILES=$(find . -maxdepth 1 -name 'cash' )
+FILES=$(find . -maxdepth 2 -name 'cash' )
 for FILE in $FILES; do
     CURRENT_CASH=$(($CURRENT_CASH + $(cat $FILE)))
 done
+echo $CURRENT_CASH
 if [ $CURRENT_CASH -ne $TOTAL_CASH ]; then
     echo 'EPIC FAIL! Nice try noob, blockchains are unhackable'
     exit 1
